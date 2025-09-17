5ea9da04b77f12c9bbdd19f91df5a6a9af1395f2
Ivanitas <iffan.chalif62@gmail.com>
2025-09-17 15:34:57 +0700

    half way complete

--- main/views.py
+++ main/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
 
 def show_xml_by_id(request, news_id):
    try:
@@ -36,6 +78,7 @@ def show_json(request):
     json_data = serializers.serialize("json", news_list)
     return HttpResponse(json_data, content_type="application/json")
 
+@login_required(login_url='/login')
 def show_main(request):
     news_list = News.objects.all()
 
@@ -43,7 +86,8 @@ def show_main(request):
         'npm' : '2406435250',
         'name': 'Iffan Chalif',
         'class': 'PBP B',
-        'news_list': news_list
+        'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -58,7 +102,8 @@ def create_news(request):
     context = {'form': form}
     return render(request, "create_news.html", context)
 
-def show_news(request, id):
+@login_required(login_url='/login')
+def show_news(request):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
 
